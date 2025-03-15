8d3f70d494e5b18a3998b474a27a52e9b89aae8c
Haici Yang <hy17@h1.quartz.uits.iu.edu>
2025-03-15 18:00:36 -0400

    cleaning

--- audiocraft/models/encodec.py
+++ audiocraft/models/encodec.py
@@ -328,56 +328,6 @@ class DAC(CompressionModel):
         assert n >= 1
         assert n <= self.total_codebooks
         self.n_quantizers = n
-
-# def decimal_to_ternary_matrix(decimals, D):
-#     """
-#     Convert a tensor of decimal numbers to a D*T ternary matrix for each batch.
-
-#     Arguments
-#     ---------
-#     decimals : torch.Tensor
-#         A 2D tensor of decimal numbers with shape (B, T), where B is the batch size
-#         and T is the number of elements in each batch.
-#     D : int
-#         Number of ternary digits to represent each number (depth).
-
-#     Returns
-#     -------
-#     torch.Tensor
-#         A 3D tensor of shape (B, D, T) where each slice along the first dimension
-#         corresponds to a batch, and each column is represented as a ternary number.
-#     """
-#     B, T = decimals.shape
-#     ternary_matrix = torch.zeros((B, D, T), dtype=torch.long)
-#     for pos in range(D):
-#         ternary_matrix[:, pos, :] = decimals % 3  # Modulo operation
-#         decimals //= 3  # Floor division for next ternary digit
-
-#     return ternary_matrix
-
-
-# def ternary_matrix_to_decimal(matrix):
-#     """
-#     Convert a D*N ternary matrix to a list of decimal numbers.
-    
-#     Parameters:
-#     - matrix: a 2D numpy array of shape (D, N) where each column is a ternary number.
-    
-#     Returns:
-#     - A list of integers, each representing the decimal equivalent of a ternary number.
-#     """
-#     B, D, N = matrix.shape  # B is the batch size, D is the number of digits, N is the number of ternary numbers
-#     # shape (1, 9, 150) values from 0, 1, 2
-    
-#     powers_of_three = 3 ** np.arange(D)  # [3^0, 3^1, ..., 3^(D-1)]
-
-#     # Reshape powers_of_three for broadcasting: [D] -> [1, D, 1]
-#     powers_of_three = powers_of_three[:, np.newaxis]  # Shape [D, 1]
-
-#     # Compute dot product using broadcasting: matrix * powers_of_three along D axis
-#     decimals = np.sum(matrix * powers_of_three, axis=1)  # Sum along the D axis
-
-#     return decimals
     
 class SQCodec(CompressionModel):
     """SQCodec adapted tokenizer version for LLM training
